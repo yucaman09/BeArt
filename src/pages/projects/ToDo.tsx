33b57c7673e1c63d9894e5bd2659db2704ba8378
Yulman <yulmanbryan009@hotmail.com>
2025-02-06 02:00:48 -0500

    Se añaden botones

--- src/pages/projects/ToDo.tsx
+++ src/pages/projects/ToDo.tsx
@@ -1,9 +1,13 @@
 import { Panel } from "primereact/panel";
+import { Button } from "primereact/button";
 
 const ToDo = () => {
   return (
     <Panel header="To-Do List">
-      
+      <div className="card flex justify-content-center gap-3">
+        <Button label="Añadir" severity="info" raised />
+        <Button label="Borrar" severity="danger" raised />
+      </div>
     </Panel>
   );
 };
